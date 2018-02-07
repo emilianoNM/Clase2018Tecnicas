e98f86f9df8f2d00844176244f8b67aa65d83d60
emiliano <enava@asimov.mx>
2018-02-06 20:57:08 -0600

    Operacion suma

---
+++ suma.py
@@ -0,0 +1,11 @@
+
+print ("Calculadora")
+
+a=input("dame  un valor a")
+b=input("dame  un valor b")
+caracter=input("dame  la operacion ")
+
+if caracter=="+" :
+	c=a+b
+
+print ("el resultado de c  es ", c)
