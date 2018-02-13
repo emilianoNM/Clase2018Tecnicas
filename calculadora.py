b7fb1db0af38e265d192e21abf44aa47de97823a
MaxxMenesess <36320237+MaxxMenesess@users.noreply.github.com>
2018-02-12 21:55:50 -0600

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,39 +1,28 @@
-print("===CALCULADORA===")
-print("Elige una opción")
-print("1-Suma")
-print("2-Resta")
-print("3-Multiplicación")
-print("4-División")
-x= input("Escribe tu elección:")
-y=float(x)
+print("Bienvenido, este programa es una calculadora")
+print("esta calculadora te permite realizar las siguientes operaciones")
+print("suma (+)")
+print("Resta (-)")
+print("multiplicacion (*)")
+print("Division(/)")
 
-if(y==1):
-    a=input("Introduce el primer numero\n")
-    b=input("Introduce el segundo numero\n")
-    a2=float(a)
-    b2=float(b)
-    resultado= a2+b2
-    print("El resultado de la suma es:",resultado)
-elif(y==2):
-    a=input("Introduce el primer numero\n")
-    b=input("Introduce el segundo numero\n")
-    a2=float(a)
-    b2=float(b)
-    resultado= a2-b2
-    print("El resultado de la resta es:",resultado)
-elif(y==3):
-    a=input("Introduce el primer numero\n")
-    b=input("Introduce el segundo numero\n")
-    a2=float(a)
-    b2=float(b)
-    resultado= a2*b2
-    print("El resultado de la multiplicación es:",resultado)
-elif(y==4):
-    a=input("Introduce el primer numero\n")
-    b=input("Introduce el segundo numero\n")
-    a2=float(a)
-    b2=float(b)
-    resultado= a2/b2
-    print("El resultado de la división es:",resultado)
-else:
-    print("NUMERO INVALIDO")
+
+a=input("ingresa el primer numero")
+b=input("ingresa el segundo numero")
+
+caracter=input("Ingresa la operacion que deseas ejecutar")
+
+if (caracter==+)
+suma=a+b
+print("el resultado de la suma es",suma)
+
+if (caracter==-)
+Resta=a-b
+print("el resultado de la resta es",resta)
+
+if (caracter==*)
+multiplicacion=a+b
+print("el resultado de la suma es",multiplicacion)
+
+if (caracter==/)
+Division=a/b
+print("el resultado de la divison es",division)
