8da23ca523f1c4d3c34fdf8505e42bec35a80de8
Halimva <36388640+Halimva@users.noreply.github.com>
2018-02-12 23:24:48 -0600

    Update CalculadoraHFF.py

--- Clase07022018/CalculadoraHFF.py
+++ Clase07022018/CalculadoraHFF.py
@@ -1,27 +1,27 @@
 print ("Calculadora basica")
 num1=int(input("Escribe el primer numero: "))
-op=input("Operacion: +, -, *, /")
+op=input("Operacion: 1 para +, 2 para -, 3 para *, 4 para /")
 num2=int(input("Escribe el segundo numero: "))
 
-if op=="+":
+if op==1:
 	res=num1+num2
 	print(res)
 else:
 	pass
 
-if op=="-":
+if op==2:
 	res=num1-num2
 	print(res)
 else:
 	pass
 
-if op=="*":
+if op==3:
 	res=num1*num2
 	print(res)
 else:
 	pass
 
-if op=="/":
+if op==4:
 	res=num1/num2
 	print(res)
 else:
