3272b4e25132319fb08d417c1184007caa3454ce
Egoragna <36322144+Egoragna@users.noreply.github.com>
2018-02-17 20:15:29 -0600

    Create ZooMM.py
    Created by Mario Molina

---
+++ Clase1402018/ZooMM.py
@@ -0,0 +1,15 @@
+#Programa hecho por Mario Molina
+
+class Especie:
+	def __init__(self):
+		self.Habitat=[None]
+		self.Sexo=[None]
+		self.Nombre=[None]
+
+class Carnivoros:
+	def __init__(self):
+		self.Comecarne=[None]
+
+class Herviboros:
+	def __init__(self):
+		self.Comehierba=[None]
