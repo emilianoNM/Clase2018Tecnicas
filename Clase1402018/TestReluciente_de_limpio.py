9b98277243a8d3a01b294f8f447087053aa00a84
Egoragna <36322144+Egoragna@users.noreply.github.com>
2018-02-18 17:01:24 -0600

    Create TestReluciente_de_limpio.py
    Created by Mario Molina

---
+++ Clase1402018/TestReluciente_de_limpio.py
@@ -0,0 +1,106 @@
+###Programa creado por Mario Molina
+# -*- coding: Windows-1252 -*-
+ 
+import sys
+print sys.stdout.encoding
+
+import Reluciente_de_limpio
+
+print("")
+
+Ropa1=Reluciente_de_limpio.Reluciente()
+Ropa1.Prenda="Blusa"
+Ropa1.Tratamiento="Lavado"
+Ropa1.Tiempo="10 minutos"
+print ("Seleccionaste la opción para: " + Ropa1.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa1.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa1.Tiempo)
+
+print("")
+
+Ropa2=Reluciente_de_limpio.Reluciente()
+Ropa2.Prenda="Pantalón"
+Ropa2.Tratamiento="Lavado"
+Ropa2.Tiempo="10 minutos"
+print ("Seleccionaste la opción para: " + Ropa2.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa2.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa2.Tiempo)
+
+print("")
+
+Ropa3=Reluciente_de_limpio.Reluciente()
+Ropa3.Prenda="Traje"
+Ropa3.Tratamiento="Lavado, secado y planchado"
+Ropa3.Tiempo="60 minutos"
+print ("Seleccionaste la opción para: " + Ropa3.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa3.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa3.Tiempo)
+
+print("")
+
+Ropa4=Reluciente_de_limpio.Reluciente()
+Ropa4.Prenda="Saco"
+Ropa4.Tratamiento="Planchado"
+Ropa4.Tiempo="15 minutos"
+print ("Seleccionaste la opción para: " + Ropa4.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa4.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa4.Tiempo)
+
+print("")
+
+Ropa5=Reluciente_de_limpio.Reluciente()
+Ropa5.Prenda="Blusa"
+Ropa5.Tratamiento="Secado y planchado"
+Ropa5.Tiempo="20 minutos"
+print ("Seleccionaste la opción para: " + Ropa5.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa5.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa5.Tiempo)
+
+print("")
+
+Ropa6=Reluciente_de_limpio.Reluciente()
+Ropa6.Prenda="Pantalón"
+Ropa6.Tratamiento="Lavado"
+Ropa6.Tiempo="10 minutos"
+print ("Seleccionaste la opción para: " + Ropa6.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa6.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa6.Tiempo)
+
+print("")
+
+Ropa7=Reluciente_de_limpio.Reluciente()
+Ropa7.Prenda="Corbata"
+Ropa7.Tratamiento="Lavado, secado y planchado"
+Ropa7.Tiempo="30 minutos"
+print ("Seleccionaste la opción para: " + Ropa7.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa7.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa7.Tiempo)
+print("")
+
+Ropa8=Reluciente_de_limpio.Reluciente()
+Ropa8.Prenda="Lote de blancos"
+Ropa8.Tratamiento="Lavado y secado"
+Ropa8.Tiempo="60 minutos"
+print ("Seleccionaste la opción para: " + Ropa8.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa8.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa8.Tiempo)
+
+print("")
+
+Ropa9=Reluciente_de_limpio.Reluciente()
+Ropa9.Prenda="Lote de color"
+Ropa9.Tratamiento="Lavado y secado"
+Ropa9.Tiempo="30 minutos"
+print ("Seleccionaste la opción para: " + Ropa9.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa9.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa9.Tiempo)
+
+print("")
+
+Ropa10=Reluciente_de_limpio.Reluciente()
+Ropa10.Prenda="Camisa"
+Ropa10.Tratamiento="Lavado"
+Ropa10.Tiempo="10 minutos"
+print ("Seleccionaste la opción para: " + Ropa10.Prenda)
+print ("Seleccionaste el siguiente tratamiento: " + Ropa10.Tratamiento)
+print ("Tu prenda estará lista en: " + Ropa10.Tiempo)
