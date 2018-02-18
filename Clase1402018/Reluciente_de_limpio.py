fa4e4cf2e98411de5ff4002021d21a9ef50a83dd
Egoragna <36322144+Egoragna@users.noreply.github.com>
2018-02-18 17:00:17 -0600

    Create Reluciente_de_limpio.py
    Created by Mario Molina

---
+++ Clase1402018/Reluciente_de_limpio.py
@@ -0,0 +1,11 @@
+###Programa creado por Mario Molina
+
+class Reluciente:
+	def __init__(self):
+		self.Prenda=[None]
+		self.Tratamiento=[None]
+		self.Tiempo=[None]
+	class Blancos:
+		pass
+	class Color:
+		pass
