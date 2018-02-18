cb0b4652eab9bfda50bfb62e87d07927ad4cf016
Egoragna <36322144+Egoragna@users.noreply.github.com>
2018-02-17 20:17:09 -0600

    Create TestZooMM.py
    Created by Mario Molina

---
+++ Clase1402018/TestZooMM.py
@@ -0,0 +1,142 @@
+#Programa hecho por Mario Molina
+
+import ZooMM
+
+print("")
+SexoM="Macho"
+SexoH="Hembra"
+Comehierba="Si"
+Comecarne="No"
+Elefante=ZooMM.Especie()
+Elefante2=ZooMM.Especie()
+Elefante=ZooMM.Carnivoros()
+Elefante=ZooMM.Herviboros()
+Elefante.Habitat="Bosque Tropical"
+Elefante.Nombre="Melvin"
+
+print ("Animal registrado: Elefante")
+print ("Habitat: " + Elefante.Habitat)
+print ("Sexo: " + SexoM)
+print ("Nombre: " + Elefante.Nombre)
+print ("Herbivoro: " + Comehierba)
+print ("Carnivoro: " + Comecarne)
+
+print("")
+
+Elefante2=ZooMM.Especie()
+Elefante2.Nombre="Melva"
+
+print ("Animal registrado: Elefante")
+print ("Habitat: " + Elefante.Habitat)
+print ("Sexo: " + SexoH)
+print ("Nombre: " + Elefante2.Nombre)
+print ("Herbivoro: " + Comehierba)
+print ("Carnivoro: " + Comecarne)
+
+print("")
+
+Leon=ZooMM.Especie()
+Leon=ZooMM.Carnivoros()
+Leon=ZooMM.Herviboros()
+Leon.Habitat="Sabana y Pastizales"
+Leon.Nombre="Simba"
+
+print ("Animal registrado: Leon")
+print ("Habitat: " + Leon.Habitat)
+print ("Sexo: " + SexoM)
+print ("Nombre: " + Leon.Nombre)
+print ("Herviboro: " + Comecarne)
+print ("Carnivoro: " + Comehierba)
+
+print("")
+
+Leon2=ZooMM.Especie()
+Leon2.Nombre="Nala"
+
+print ("Animal registrado: Leon")
+print ("Habitat: " + Leon.Habitat)
+print ("Sexo: " + SexoH)
+print ("Nombre: " + Leon2.Nombre)
+print ("Herviboro: " + Comecarne)
+print ("Carnivoro: " + Comehierba)
+
+print("")
+
+Jirafa=ZooMM.Especie()
+Jirafa=ZooMM.Carnivoros()
+Jirafa=ZooMM.Herviboros()
+Jirafa.Habitat="Bosques y LLanuras abiertas"
+Jirafa.Nombre="Melman"
+
+print ("Animal registrado: Jirafa")
+print ("Habitat: " + Jirafa.Habitat)
+print ("Sexo: " + SexoM)
+print ("Nombre: " + Jirafa.Nombre)
+print ("Herviboro: " + Comehierba)
+print ("Carnivoro: " + Comecarne)
+
+print("")
+
+Jirafa2=ZooMM.Especie()
+Jirafa2.Nombre="Zira"
+
+print ("Animal registrado: Jirafa")
+print ("Habitat: " + Jirafa.Habitat)
+print ("Sexo: " + SexoH)
+print ("Nombre: " + Jirafa2.Nombre)
+print ("Herviboro: " + Comehierba)
+print ("Carnivoro: " + Comecarne)
+
+print("")
+
+Hipopotamo=ZooMM.Especie()
+Hipopotamo=ZooMM.Carnivoros()
+Hipopotamo=ZooMM.Herviboros()
+Hipopotamo.Habitat="Lagos y Rios"
+Hipopotamo.Nombre="Pluna"
+
+print ("Animal registrado: Hipopotamo")
+print ("Habitat: " + Hipopotamo.Habitat)
+print ("Sexo: " + SexoM)
+print ("Nombre: " + Hipopotamo.Nombre)
+print ("Herviboro: " + Comehierba)
+print ("Carnivoro: " + Comehierba)
+
+print("")
+
+Hipopotamo2=ZooMM.Especie()
+Hipopotamo2.Nombre="Kara"
+
+print ("Animal registrado: Hipopotamo")
+print ("Habitat: " + Hipopotamo.Habitat)
+print ("Sexo: " + SexoH)
+print ("Nombre: " + Hipopotamo2.Nombre)
+print ("Herviboro: " + Comehierba)
+print ("Carnivoro: " + Comehierba)
+
+print("")
+
+Cebra=ZooMM.Especie()
+Cebra=ZooMM.Carnivoros()
+Cebra=ZooMM.Herviboros()
+Cebra.Habitat="Sabana"
+Cebra.Nombre="Marvin"
+
+print ("Animal registrado: Cebra")
+print ("Habitat: " + Cebra.Habitat)
+print ("Sexo: " + SexoM)
+print ("Nombre: " + Cebra.Nombre)
+print ("Herviboro: " + Comehierba)
+print ("Carnivoro: " + Comecarne)
+
+print("")
+
+Cebra2=ZooMM.Especie()
+Cebra2.Nombre="Kitara"
+
+print ("Animal registrado: Cebra")
+print ("Habitat: " + Cebra.Habitat)
+print ("Sexo: " + SexoH)
+print ("Nombre: " + Cebra2.Nombre)
+print ("Herviboro: " + Comehierba)
+print ("Carnivoro: " + Comecarne)
