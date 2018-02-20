adb5ef60418dd63b91d48058d67ce9d04e1c5705
alvert1996 <36251909+alvert1996@users.noreply.github.com>
2018-02-20 00:17:11 -0600

    Update lavanderia_alvert1996.py

--- Clase1402018/lavanderia_alvert1996.py
+++ Clase1402018/lavanderia_alvert1996.py
@@ -1,21 +1,57 @@
 #Alberto Garcia Salazar
 
-class lavanderia:
+class Lavanderia:
 
-	def _init_(sellf)
-		self.color=[None]
-		self.tipo=[None]
-		self.costo=[None]
+	color=[None]
+	tipo=[None]
+	costo=[None]
 
-class lavado(lavanderia):
+
+	def __init__(self,color,tipo,costo):
+		self.color=color
+		self.tipo=tipo
+		self.costo=costo
+
+class lavado(Lavanderia):
+	agua=None
+	detergente=None
+	suavizante=None
+	def __init__(self,color,tipo,costo,agua,detergente,suavizante):
+		Lavanderia.__init__(self,color,tipo,costo)
+		self.agua=agua
+		self.detergente=detergente
+		self.suavizante=suavizante
 	pass
 
-class secado(lavanderia):
+class secado(Lavanderia):
+	capacidad=None
+	tiempo=None
+	modo=None
+	def __init__(self,color,tipo,costo,capacidad,tiempo,modo):
+		Lavanderia.__init__(self,color,tipo,costo)
+		self.capacidad=capacidad
+		self.tiempo=tiempo
+		self.modo=modo
 	pass
 
-class espera(lavanderia);
+class espera(Lavanderia):
+	capacidadp=None
+	elementos=None
+	ubicacion=None
+	def __init__(self,color,tipo,costo,capacidadp,elementos,ubicacion):
+		Lavanderia.__init__(self,color,tipo,costo)
+		self.capacidadp=capacidadp
+		self.elementos=elementos
+		self.ubicacion=ubicacion
 	pass
 
-class atencion(lavanderia):
+class atencion(Lavanderia):
+	personas=None
+	elementosa=None
+	def __init__(self,color,tipo,costo,personas,elementosa):
+		Lavanderia.__init__(self,color,tipo,costo)
+		self.personas=personas
+		self.elementosa=elementosa
 	pass
+
 
