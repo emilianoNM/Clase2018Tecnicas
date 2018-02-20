4f072ac47ed0575a5289383da50eb23b69bdb03b
alvert1996 <36251909+alvert1996@users.noreply.github.com>
2018-02-19 22:22:07 -0600

    Update Zoologico_alvert1996.py

--- Clase1402018/Zoologico_alvert1996.py
+++ Clase1402018/Zoologico_alvert1996.py
@@ -1,20 +1,68 @@
 #Alberto Garcia Salazar
 
 class Especies:
-	
-	def _init_(self):
-		self.estado=[None]
-		self.sexo=[None]
-		self.color=[None]		
+	estado=[]	
+	edad=[]
+	color=[]
+	nombre=[]
+
+
+	def __init__(self,estado,edad,color,nombre):
+		self.estado=estado
+		self.edad=edad
+		self.color=color		
+		self.nombre=nombre
 
 class tucan(Especies):
+	pico=None
+	pluma=None
+	canto=None
+	def __init__(self,estado,edad,color,nombre,pico,pluma,canto):
+		Especies.__init__(self,estado,edad,color,nombre)
+		self.pico=pico
+		self.pluma=pluma
+		self.canto=canto
 	pass
 
 class cocodrilo(Especies):
+	tamano=None
+	piel="escamosa"
+	comida=None
+	def __init__(self,estado,edad,color,nombre,tamano,piel,comida="Carne"):
+		Especies.__init__(self,estado,edad,color,nombre)
+		self.tamano=tamano
+		self.comida="carne"
 	pass
 
 class elefante(Especies):
+	tamano=None
+	piel=None
+	sexo=None
+	def __init__(self,estado,edad,color,nombre,tamano,piel,sexo):
+		Especies.__init__(self,estado,edad,color,nombre)
+		self.tamano=tamano
+		self.piel=piel
+		self.sexo=sexo
 	pass
 
 class jirafa(Especies):
+	comida=None
+	piel=[]
+	sexo=[]
+	def __init__(self,estado,edad,color,nombre,comida,piel,sexo):
+		Especies.__init__(self,estado,edad,color,nombre)
+		self.comida=comida
+		self.piel=piel
+		self.sexo=sexo
+	pass
+
+class hipopotamo(Especies):
+	sexo=[]
+	sonido=[]
+	caracter=[]
+	def __init__(self,estado,edad,color,nombre,sonido,caracter,sexo):
+		Especies.__init__(self,estado,edad,color,nombre)
+		self.sonido=sonido
+		self.caracter=caracter
+		self.sexo=sexo
 	pass
