f4741f0a9b4f9fd3da7364f822517cce892f99ea
alvert1996 <36251909+alvert1996@users.noreply.github.com>
2018-02-20 00:18:40 -0600

    Update Testlavanderia_alvert1996.py

--- Clase1402018/Testlavanderia_alvert1996.py
+++ Clase1402018/Testlavanderia_alvert1996.py
@@ -1,55 +1,33 @@
 #ALberto Garcia SAlazar
 
-class Lavanderia:
+import lavanderia_alvert1996
 
-	color=[None]
-	tipo=[None]
-	costo=[None]
+s2102=lavanderia_alvert1996.lavado("gris","electronica","$7,500 pesos","13 litros","azul","'borrego'")
 
+print("\nElementos: "+str(s2102))
+print("Esta lavadora es "+s2102.color+" es "+s2102.tipo+" con un costo de "+s2102.costo)
+print("Ocupa "+s2102.agua+" de agua, detergente "+s2102.detergente+" y suavizante de marca"+s2102.suavizante)
 
-	def __init__(self,color,tipo,costo):
-		self.color=color
-		self.tipo=tipo
-		self.costo=costo
+banca=lavanderia_alvert1996.espera("cafe","descanso","$1,300 pesos","4 personas","3","afuera")
 
-class lavado(Lavanderia):
-	agua=None
-	detergente=None
-	suavizante=None
-	def __init__(self,color,tipo,costo,agua,detergente,suavizante):
-		Lavanderia.__init__(self,color,tipo,costo)
-		self.agua=agua
-		self.detergente=detergente
-		self.suavizante=suavizante
-	pass
+print("\nElemento: "+str(banca))
+print("Esta banca es "+banca.color+" es para "+banca.tipo+" con un costo de "+banca.costo)
+print("Tiene una capacidad para "+banca.capacidadp+", hay "+banca.elementos+" y se encuentran "+banca.ubicacion)
 
-class secado(Lavanderia):
-	capacidad=None
-	tiempo=None
-	modo=None
-	def __init__(self,color,tipo,costo,capacidad,tiempo,modo):
-		Lavanderia.__init__(self,color,tipo,costo)
-		self.capacidad=capacidad
-		self.tiempo=tiempo
-		self.modo=modo
-	pass
+recepcion=lavanderia_alvert1996.atencion("blanco","atencion al usuario","$3,500 pesos","3 personas","1")
 
-class espera(Lavanderia):
-	capacidadp=None
-	elementos=None
-	ubicacion=None
-	def __init__(self,color,tipo,costo,capacidadp,elementos,ubicacion):
-		Lavanderia.__init__(self,color,tipo,costo)
-		self.capacidadp=capacidadp
-		self.elementos=elementos
-		self.ubicacion=ubicacion
-	pass
+print("\nElemento: "+str(recepcion))
+print("Este mobiliario es "+recepcion.color+" es para "+recepcion.tipo+" con un costo de "+recepcion.costo)
+print("Estan presentes "+recepcion.personas+" y solo hay "+recepcion.elementosa+" en el lugar")
 
-class atencion(Lavanderia):
-	personas=None
-	elementosa=None
-	def __init__(self,color,tipo,costo,personas,elementosa):
-		Lavanderia.__init__(self,color,tipo,costo)
-		self.personas=personas
-		self.elementosa=elementosa
-	pass
+sillon=lavanderia_alvert1996.espera("negro","descanso","$4,500 pesos","3 personas","2","adentro")
+
+print("\nElemento: "+str(sillon))
+print("Este sillon es "+sillon.color+" es para "+sillon.tipo+" con un costo de "+sillon.costo)
+print("Tiene una capacidad para "+sillon.capacidadp+", hay "+sillon.elementos+" y se encuentran "+sillon.ubicacion+" del lugar")
+
+sle21=lavanderia_alvert1996.secado("blanca","electronica","$6,400 pesos","10 kilos","30 minutos","automaico")
+
+print("\nElemento: "+str(sle21))
+print("Esta secadora es "+sle21.color+", "+sle21.tipo+" con un costo de "+sle21.costo)
+print("Tiene una capcidad de "+sle21.capacidad+" y su tiempo de trabajo es de "+sle21.tiempo+" con su modo "+sle21.modo)
