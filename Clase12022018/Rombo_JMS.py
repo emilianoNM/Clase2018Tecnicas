7d2d98af83debd444eec3d709a344f585ba72c7b
MaxxMenesess <36320237+MaxxMenesess@users.noreply.github.com>
2018-02-21 20:48:43 -0600

    Update Rombo_JMS.py

--- Clase12022018/Rombo_JMS.py
+++ Clase12022018/Rombo_JMS.py
@@ -1,3 +1,25 @@
 #Meneses Silva Maximino Javier
 print("Este programa imprime un Rombo")
 print ("")
+dim=int(input("introduce un numero:"))
+if dim<=2:
+ print("El rombo no puede ser dibujado, es muy pequeño")
+
+else:
+ for i in range (dim,0,-1):
+    for j in range(dim,(dim-(i+1)),-1):
+        print(" ",end= "")
+    for k in range (i+1,dim+1):
+        print("*",end= "") 
+    print("")
+ for i in range(0,dim):
+
+     for j in range(dim,(dim-(i+1)),1):
+
+          print(" ", end="")
+ 
+     for k in range(i+1,dim+1):
+
+          print("*",end="")
+
+     print("")
