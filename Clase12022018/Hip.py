4ceaf7f2515ebd866c84aafe95b1d26b549561f5
OliverMendieta <36421821+OliverMendieta@users.noreply.github.com>
2018-03-06 14:16:11 -0600

    Add files via upload

---
+++ Clase12022018/Hip.py
@@ -0,0 +1,28 @@
+print "Tecnicas de programacion"
+print "Mendieta Montoya Oliver Salvador"
+print "Usuario github OliverMendieta\n"
+
+print "Hipotenusa de un Triangulo\n"
+
+CatOp = float(input("Dame un valor para cateto opuesto\n"))
+CatAb = float(input("Dame un valor para cateto abyacente\n"))
+
+if CatOp != CatAb and CatOp>0 and CatAb>0:
+	
+
+	hipotenusa = ((CatOp**2)+(CatAb**2))**1/2
+
+	print hipotenusa 
+	
+else :
+	print "Los valores no corresponden a un los catetos de un triangulo rectangulo"
+
+
+
+
+
+
+
+1	
+ 
+
