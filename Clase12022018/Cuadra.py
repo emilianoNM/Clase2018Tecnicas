9256d2e5cfadfe43d4e48a3c6c0c5270675a69d2
OliverMendieta <36421821+OliverMendieta@users.noreply.github.com>
2018-03-06 14:16:59 -0600

    Add files via upload

---
+++ Clase12022018/Cuadra.py
@@ -0,0 +1,15 @@
+print "Tecnicas de programacion"
+print "Mendieta Montoya Oliver Salvador"
+print "Usuario github OliverMendieta\n"
+print ("Cuadrado en ascci")
+
+c = int(input("numero de columnas:")) 
+r = int(input("numero de renglones"))
+
+for i in range (1,r+1):
+	for j in range (1,1+c):
+		n = chr (43)
+		print n,
+	
+	print " "
+
