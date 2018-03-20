529c367e0491e48377eec1940209f3f1016e92e3
MaxxMenesess <36320237+MaxxMenesess@users.noreply.github.com>
2018-03-20 17:26:08 -0600

    Update ZOO2_MJMS.py

--- Clase19022018/ZOO2_MJMS.py
+++ Clase19022018/ZOO2_MJMS.py
@@ -1,1 +1,249 @@
-print("Zoologico")
+class Especie():
+      Nombre=None
+      Alimentacion=None
+      Edad=None
+
+class Rinoceronte(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print("Datos del animal 1:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"El rinoceronte",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+
+class Cebra(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print('\n'"Datos del animal 2:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"La cebra",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+class Gorilla(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print('\n'"Datos del animal 3:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"El gorilla",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+
+class Leon(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print('\n'"Datos del animal 4:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"El leon",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+
+class Cocodrilo(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print('\n'"Datos del animal 5:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"El cocodrilo",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+
+class Hipopotamo(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print('\n'"Datos del animal 6:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"El hipopotamo",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+
+class Jirafa(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print('\n'"Datos del animal 7:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"La jirafa",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+
+class Elefante(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print('\n'"Datos del animal 8:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"El elefante",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+class Pato(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print('\n'"Datos del animal 9:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"El pato",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+
+class Jaguar(Especie):
+      Tamano=None
+      Pasatiempo=None
+      Amigable=None 
+      
+      def __init__(self,Nombre,Alimentacion,Edad, Tamano, Pasatiempo, Amigable):
+            self.Nombre=Nombre
+            self.Alimentacion=Alimentacion
+            self.Edad=Edad
+            self.Tamano=Tamano
+            self.Pasatiempo=Pasatiempo
+            self.Amigable=Amigable
+      
+      def __str__(self):
+         return self.Nombre
+ 
+      def Datos(self):
+         print('\n'"Datos del animal 10:")
+         print("Nombre:",str(self.Nombre),'\n'"Alimentacion:",str(self.Alimentacion),'\n'"Edad:",str(self.Edad)) 
+      
+      def Caracter(self) :
+         print('\n'"El jaguar",str(self),"es",str(self.Tamano),", Su pasatiempo favorito es", str(self.Pasatiempo), "y",str(self.Amigable))
+
+
+
+
+
+
+
+
