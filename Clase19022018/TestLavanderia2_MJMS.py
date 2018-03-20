9db8aa90e2142cb892dde0cac3b1dc1d5942a430
MaxxMenesess <36320237+MaxxMenesess@users.noreply.github.com>
2018-03-20 17:39:24 -0600

    Update TestLavanderia2_MJMS.py

--- Clase19022018/TestLavanderia2_MJMS.py
+++ Clase19022018/TestLavanderia2_MJMS.py
@@ -1,2 +1,42 @@
-print("Test Lavanderia")
+#Meneses Silva Maximino Javier
 import Lavanderia2_MJMS
+
+Prenda1=Lavanderia2_MJMS.Prenda1("Jeans ","Muy sucios","Color","Detergente liquido","\nLavado \nSecado","algodón")
+Prenda1.Info()
+Prenda1.Lavamos()
+
+Prenda2=Lavanderia2_MJMS.Prenda2("Camisa ","Poco sucia","Color","Detergente liquido","\nLavado \nSecado \nPlanchado ","algodón")
+Prenda2.Info()
+Prenda2.Lavamos()
+
+Prenda3=Lavanderia2_MJMS.Prenda3("Playera ","Muy sucia","Blanca","Detergente en polvo","\nLavado \nSecado","algodón")
+Prenda3.Info()
+Prenda3.Lavamos()
+
+Prenda4=Lavanderia2_MJMS.Prenda4("Chamarra","Poco sucia","Color","Detergente liquido","\nLavado \nSecado","poliéster")
+Prenda4.Info()
+Prenda4.Lavamos()
+
+Prenda5=Lavanderia2_MJMS.Prenda5("Playera deportiva ","sucia","Color","Detergente en polvo","\nLavado \nSecado","poliéster")
+Prenda5.Info()
+Prenda5.Lavamos()
+
+Prenda6=Lavanderia2_MJMS.Prenda6("Calcetines ","Muy sucios","Blanco","Detergente en polvo","\nLavado \nSecado","algodon")
+Prenda6.Info()
+Prenda6.Lavamos()
+
+Prenda7=Lavanderia2_MJMS.Prenda7("Bata ","Demasiado sucioa","Blanco","Detergente liquido","\nLavado \nSecado \nPlanchado ","algodon")
+Prenda7.Info()
+Prenda7.Lavamos()
+
+Prenda8=Lavanderia2_MJMS.Prenda8("Sabana ","Poco sucia","Color","Detergente en polvo","\nLavado \nSecado","algodon")
+Prenda8.Info()
+Prenda8.Lavamos()
+
+Prenda9=Lavanderia2_MJMS.Prenda9("Cobija ","Muy sucia","Color","Detergente en polvo","\nLavado \nSecado","algodon")
+Prenda9.Info()
+Prenda9.Lavamos()
+
+Prenda10=Lavanderia2_MJMS.Prenda10("Ropa de bebé ","Poco sucioa","Color","Detergente liquido y suavizante","\nLavado \nSecado \nPlanchado ","algodon")
+Prenda10.Info()
+Prenda10.Lavamos()
