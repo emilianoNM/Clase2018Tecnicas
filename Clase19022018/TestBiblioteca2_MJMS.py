923e80d92f0fe1193cbd0eae4998467b8f706922
MaxxMenesess <36320237+MaxxMenesess@users.noreply.github.com>
2018-03-20 17:33:18 -0600

    Update TestBiblioteca2_MJMS.py

--- Clase19022018/TestBiblioteca2_MJMS.py
+++ Clase19022018/TestBiblioteca2_MJMS.py
@@ -1,2 +1,43 @@
-print("Test Biblioteca")
+#Meneses Silva Maximino Javier
 import Biblioteca2_MJMS
+
+Libro1=Biblioteca2_MJMS.Mate("Cálculo diferencial e integral","2","412","Si","Frank Ayres, Jr.","Matematicas ","35")
+Libro1.Datos()
+Libro1.Tulibro()
+
+Libro2=Biblioteca2_MJMS.Novelas("Cien años de soledad","1","432","No","Gabriel García Márquez","Novelas ","20")
+Libro2.Datos()
+Libro2.Tulibro()
+
+Libro3=Biblioteca2_MJMS.Historia("100 minutos de la historia de México","3","200","Si","Francisco Martín Moreno","Historia ","12")
+Libro3.Datos()
+Libro3.Tulibro()
+
+Libro4=Biblioteca2_MJMS.Fisica("Física Cuántica","1","501","Si","Albert Einstein.","Física ","24")
+Libro4.Datos()
+Libro4.Tulibro()
+
+Libro5=Biblioteca2_MJMS.Quimica("Química General","6","542","Si","Ralph H. Petrucci","Química","32")
+Libro5.Datos()
+Libro5.Tulibro()
+
+Libro6=Biblioteca2_MJMS.Medicina("Principios de anatomía y fisiología","9","412","Si","Gérard J. Tortora","Medicina ",50)
+Libro6.Datos()
+Libro6.Tulibro()
+
+Libro7=Biblioteca2_MJMS.Poesia("Cien sonetos de amor","1","124","No","Pablo Neruda","Poesía","13")
+Libro7.Datos()
+Libro7.Tulibro()
+
+Libro8=Biblioteca2_MJMS.Cienciasaplicadas("Termodinámica","8","1000","Si","Yunus A. Çengel, Michael A. Boles.","Ciencias ","36")
+Libro8.Datos()
+Libro8.Tulibro()
+
+Libro9=Biblioteca2_MJMS.Revistas("National Geographic","SE","69","No","NG","Divulgacion Científica","NE")
+Libro9.Datos()
+Libro9.Tulibro()
+
+Libro10=Biblioteca2_MJMS.Cuentos("Pedro Paramo","7","79","Si","Juan Rulfo","Cuentos ","6")
+Libro10.Datos()
+Libro10.Tulibro()
+
