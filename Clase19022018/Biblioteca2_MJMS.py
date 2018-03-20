f655b3d6a64137c75c066276220684ee536e4f54
MaxxMenesess <36320237+MaxxMenesess@users.noreply.github.com>
2018-03-20 17:30:37 -0600

    Update Biblioteca2_MJMS.py

--- Clase19022018/Biblioteca2_MJMS.py
+++ Clase19022018/Biblioteca2_MJMS.py
@@ -1,2 +1,269 @@
-Print("Biblioteca")
+#Meneses Silva Maximino Javier
+class Biblioteca():
+      Titulo=None
+      Edicion=None
+      Paginas=None
+      Disponible=None   
 
+class Mate(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 1:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
+
+class Novelas(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 2:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
+
+class Historia(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 3:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
+
+class Fisica(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 4:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
+
+class Quimica(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 5:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
+
+class Medicina(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 6:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
+
+class Poesia(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 7:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
+
+class Cienciasaplicadas(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 8:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
+
+
+class Revistas(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 9:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
+
+
+
+class Cuentos(Biblioteca):
+      Autor=None
+      Seccion=None
+      Capitulos=None 
+      
+      
+      def __init__(self,Titulo,Edicion,Paginas,Disponible,Autor,Seccion,Capitulos):
+            self.Titulo=Titulo
+            self.Edicion=Edicion
+            self.Paginas=Paginas
+            self.Disponible=Disponible
+            self.Autor=Autor
+            self.Seccion=Seccion
+            self.Capitulos=Capitulos
+      
+      def __str__(self):
+         return self.Titulo
+ 
+      def Datos(self):
+         print('\n'"Datos del libro 10:")
+         print("Título:",str(self.Titulo),'\n'"Edición:",str(self.Edicion),'\n'"Número de páginas:",str(self.Paginas),'\n'"Disponible para prestamo:",str(self.Disponible)) 
+      
+      def Tulibro(self) :
+         print('\n'"El libro ",str(self),"del autor",str(self.Autor),", pertenece a la sección de", str(self.
+Seccion),"y tiene",str(self.Capitulos),"capítulos")
