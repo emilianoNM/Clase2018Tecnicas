fd2e02ff321de91c65d0eb26a8a23cc988a67be3
MaxxMenesess <36320237+MaxxMenesess@users.noreply.github.com>
2018-03-20 17:37:53 -0600

    Update Lavanderia2_MJMS.py

--- Clase19022018/Lavanderia2_MJMS.py
+++ Clase19022018/Lavanderia2_MJMS.py
@@ -1,1 +1,269 @@
-Print("Lavanderia")
+#Meneses Silva Maximino Javier
+class Lavanderia():
+      Prenda=None
+      Suciedad=None
+      Color=None
+      
+
+class Prenda1(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 1:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self),"se lavaran con",str(self.Detergente),"debido a que son de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
+  
+
+class Prenda2(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 2:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tu", str(self),"se lavara con",str(self.Detergente),"debido a que es de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
+class Prenda3(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 3:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tu", str(self),"se lavara con",str(self.Detergente),"debido a que es de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
+class Prenda4(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 4:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tu", str(self),"se lavara con",str(self.Detergente),"debido a que es de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
+class Prenda5(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 5:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tu", str(self),"se lavara con",str(self.Detergente),"debido a que es de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
+class Prenda6(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 6:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self),"se lavaran con",str(self.Detergente),"debido a que son de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
+class Prenda7(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 7:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tu", str(self),"se lavara con",str(self.Detergente),"debido a que es de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
+class Prenda8(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 8:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tu", str(self),"se lavara con",str(self.Detergente),"debido a que es de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
+class Prenda9(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 9:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tu", str(self),"se lavara con",str(self.Detergente),"debido a que es de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
+class Prenda10(Lavanderia):
+      Detergente=None
+      Servicio=None
+      Tipotejido=None 
+      
+      def __init__(self,Prenda,Suciedad,Color,Detergente,Servicio,Tipotejido):
+            self.Prenda=Prenda
+            self.Suciedad=Suciedad
+            self.Color=Color
+            self.Detergente=Detergente
+            self.Servicio=Servicio
+            self.Tipotejido=Tipotejido
+            
+      
+      def __str__(self):
+         return self.Prenda
+ 
+      def Info(self):
+         print('\n'"Características de tu prenda 10:")
+         print("Prenda:",str(self.Prenda),'\n'"Nivel de suciedad:",str(self.Suciedad),'\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tu", str(self),"se lavara con",str(self.Detergente),"debido a que es de",str(self.Tipotejido))
+         print('\n'"Elegiste los siguientes servicios:",str(self.Servicio))
+
