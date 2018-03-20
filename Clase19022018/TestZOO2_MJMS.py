a4aefc760198a7b7c06c0733f8d187de6d1af333
MaxxMenesess <36320237+MaxxMenesess@users.noreply.github.com>
2018-03-20 17:27:35 -0600

    Update TestZOO2_MJMS.py

--- Clase19022018/TestZOO2_MJMS.py
+++ Clase19022018/TestZOO2_MJMS.py
@@ -1,2 +1,47 @@
-print("TEst")
 import ZOO2_MJMS
+
+rocko=ZLM.Rinoceronte("Rocko","hierva","12","grande","romper cosas"," no es muy amigable")
+rocko.Datos()
+rocko.Caracter()
+
+Martin=ZLM.Cebra("Martín","hierva","15","grande","correr","es muy amigable")
+Martin.Datos()
+Martin.Caracter()
+
+
+Bruno=ZLM.Gorilla("Bruno","frutos","18","grande","comer bananas","no es muy amigable")
+Bruno.Datos()
+Bruno.Caracter()
+
+
+Alex=ZLM.Leon("Alex","carne","12","grande","cazar","no es muy amigable")
+Alex.Datos()
+Alex.Caracter()
+
+Coco=ZLM.Cocodrilo("Coco","carne","11","grande","nadar","no es muy amigable")
+Coco.Datos()
+Coco.Caracter()
+
+Gloria=ZLM.Hipopotamo("Gloria","hierva","10","grande","comer ","es amigable")
+Gloria.Datos()
+Gloria.Caracter()
+
+Julia=ZLM.Jirafa("Julia","hierva","20","muy muy grande","caminar","es muy amigable")
+Julia.Datos()
+Julia.Caracter()
+
+Dumbo=ZLM.Elefante("Dumbo","hierva","17","muy grande","bañarse","es amigable")
+Dumbo.Datos()
+Dumbo.Caracter()
+
+Paty=ZLM.Pato("Paty","insectos","2","pequeño","nadar","es muy amigable")
+Paty.Datos()
+Paty.Caracter()
+
+Nikte=ZLM.Jaguar("Nikté","carne","16","grande","cazar","no es muy amigable")
+Nikte.Datos()
+Nikte.Caracter()
+
+
+
+
