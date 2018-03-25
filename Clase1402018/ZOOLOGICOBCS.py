7c15beec078d2492d28c95a911c3b9e8565348ba
Benja1995 <36357893+Benja1995@users.noreply.github.com>
2018-03-24 21:33:10 -0600

    Add files via upload

---
+++ Clase1402018/ZOOLOGICOBCS.py
@@ -0,0 +1,11 @@
+#Benjamin Cruz Sarmiento
+
+class ZOOBCS():
+
+	def __init__(self,Animal,Genero,Tamano,Edaddevida):
+
+		self.Animal=Animal
+		self.Genero=Genero
+		self.Tamano=Tamano
+		self.Edaddevida=Edaddevida
+
