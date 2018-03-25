f7b9bac17cf8458765687280e60da58e6e7aa8fc
Benja1995 <36357893+Benja1995@users.noreply.github.com>
2018-03-24 21:31:51 -0600

    Add files via upload

---
+++ Clase1402018/CuartolavadoBCS.py
@@ -0,0 +1,9 @@
+#Benjamin Cruz Sarmiento
+
+class Lavado():
+
+	def __init__(self,Costo,Detergente,Disponible,Cantidad):
+		self.Costo=Costo
+		self.Detergente=Detergente
+		self.Disponible=Disponible
+		self.Cantidad=Cantidad
---
+++ Clase1402018/testlavadoBCS.py
@@ -0,0 +1,97 @@
+#Benjamin Cruz Sarmiento
+
+import CuartolavadoBCS
+
+Articulo1=CuartolavadoBCS.Lavado(30,"Mas Color","si",10)
+
+print("El costo por lavar es:", Articulo1.Costo," pesos")
+print("La marca del detergente es: ",Articulo1.Detergente)
+print("La lavadora 1 esta disponible : ",Articulo1.Disponible)
+print("Maxima cantidad a lavar: ",Articulo1.Cantidad,"Kilogramos")
+print("")
+print("")
+
+Articulo2=CuartolavadoBCS.Lavado(25,"Ariel","no",10)
+
+print("El costo por lavar es:", Articulo2.Costo," pesos")
+print("La marca del detergente es: ",Articulo2.Detergente)
+print("La lavadora 2 esta disponible : ",Articulo2.Disponible)
+print("Maxima cantidad a lavar: ",Articulo2.Cantidad,"Kilogramos")
+print("")
+print("")
+
+Articulo3=CuartolavadoBCS.Lavado(60,"Accion","si",12)
+
+print("El costo por lavar es:", Articulo3.Costo," pesos")
+print("La marca del detergente es: ",Articulo3.Detergente)
+print("La lavadora 3 esta disponible : ",Articulo3.Disponible)
+print("Maxima cantidad a lavar: ",Articulo3.Cantidad,"Kilogramos")
+print("")
+print("")
+
+Articulo4=CuartolavadoBCS.Lavado(67,"Maestro limpio","si",14)
+
+print("El costo por lavar es:", Articulo4.Costo," pesos")
+print("La marca del detergente es: ",Articulo4.Detergente)
+print("La lavadora 4 esta disponible : ",Articulo4.Disponible)
+print("Maxima cantidad a lavar: ",Articulo4.Cantidad,"Kilogramos")
+print("")
+print("")
+
+Articulo5=CuartolavadoBCS.Lavado(32,"Blanca Nieves","no",8)
+
+print("El costo por lavar es:", Articulo5.Costo," pesos")
+print("La marca del detergente es: ",Articulo5.Detergente)
+print("La lavadora 5 esta disponible : ",Articulo5.Disponible)
+print("Maxima cantidad a lavar: ",Articulo5.Cantidad,"Kilogramos")
+print("")
+print("")
+
+Articulo6=CuartolavadoBCS.Lavado(46,"Roma","si",9)
+
+print("El costo por lavar es:", Articulo6.Costo," pesos")
+print("La marca del detergente es: ",Articulo6.Detergente)
+print("La lavadora 6 esta disponible : ",Articulo6.Disponible)
+print("Maxima cantidad a lavar: ",Articulo6.Cantidad,"Kilogramos")
+print("")
+print("")
+
+Articulo7=CuartolavadoBCS.Lavado(17,"Espumin","no",11)
+
+print("El costo por lavar es:", Articulo7.Costo," pesos")
+print("La marca del detergente es: ",Articulo7.Detergente)
+print("La lavadora 7 esta disponible : ",Articulo7.Disponible)
+print("Maxima cantidad a lavar: ",Articulo7.Cantidad,"Kilogramos")
+print("")
+print("")
+
+Articulo8=CuartolavadoBCS.Lavado(48,"Salvo","si",3)
+
+print("El costo por lavar es:", Articulo8.Costo," pesos")
+print("La marca del detergente es: ",Articulo8.Detergente)
+print("La lavadora 8 esta disponible : ",Articulo8.Disponible)
+print("Maxima cantidad a lavar: ",Articulo8.Cantidad,"Kilogramos")
+print("")
+print("")
+
+Articulo9=CuartolavadoBCS.Lavado(38,"Zote","no",5)
+
+print("El costo por lavar es:", Articulo9.Costo," pesos")
+print("La marca del detergente es: ",Articulo9.Detergente)
+print("La lavadora 9 esta disponible : ",Articulo9.Disponible)
+print("Maxima cantidad a lavar: ",Articulo9.Cantidad,"Kilogramos")
+print("")
+print("")
+
+Articulo10=CuartolavadoBCS.Lavado(53,"Velrosita","si",24)
+
+print("El costo por lavar es:", Articulo10.Costo," pesos")
+print("La marca del detergente es: ",Articulo10.Detergente)
+print("La lavadora 10 esta disponible : ",Articulo10.Disponible)
+print("Maxima cantidad a lavar: ",Articulo10.Cantidad,"Kilogramos")
+print("")
+print("")
+
+
+
+
