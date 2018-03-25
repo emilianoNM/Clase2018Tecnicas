d7bf3633ed4d3b81fcdcec87e1425a29cc54d2b3
Benja1995 <36357893+Benja1995@users.noreply.github.com>
2018-03-24 21:30:02 -0600

    Add files via upload

---
+++ Clase1402018/BibliotecaBCS.py
@@ -0,0 +1,10 @@
+#Crear una biblioteca con 10 objetos y 3 atributos
+
+class BibliotecaUNAM():
+
+	def __init__(self,Titulo,Autor,Precio):
+		self.Titulo=Titulo
+		self.Autor=Autor
+		self.Precio=Precio
+
+
---
+++ Clase1402018/testbibliotecaBCS.py
@@ -0,0 +1,78 @@
+#Benjamin Cruz Sarmiento
+
+import BibliotecaBCS
+
+
+
+#Definiendo a los libros y sus atributos
+Libro1=BibliotecaBCS.BibliotecaUNAM("Mi padre","Sri Kahan Freud",60)
+print("Nombre del libro: ",Libro1.Titulo)
+print("El autor es: ",Libro1.Autor)
+print("Costo del libro es: ",Libro1.Precio,"pesos")
+print("")
+print("")
+
+Libro2=BibliotecaBCS.BibliotecaUNAM("El Alquimista","Paulo Cohelo",120)
+print("Nombre del libro: ",Libro2.Titulo)
+print("El autor es: ",Libro2.Autor)
+print("Costo del libro es: ",Libro2.Precio,"pesos")
+print("")
+print("")
+
+Libro3=BibliotecaBCS.BibliotecaUNAM("El almohadon de plumas","Horacio Quirog",75)
+print("Nombre del libro: ",Libro3.Titulo)
+print("El autor es: ",Libro3.Autor)
+print("Costo del libro es: ",Libro3.Precio,"pesos")
+print("")
+print("")
+
+Libro4=BibliotecaBCS.BibliotecaUNAM("El monje que vendio su ferrari","Robin Sharman",100)
+print("Nombre del libro: ",Libro4.Titulo)
+print("El autor es: ",Libro4.Autor)
+print("Costo del libro es : ",Libro4.Precio,"pesos")
+print("")
+print("")
+
+Libro5=BibliotecaBCS.BibliotecaUNAM("El cerdo capitalista","Sofia Macias",230)
+print("Nombre del libro: ",Libro5.Titulo)
+print("El autor es: ",Libro5.Autor)
+print("Costo del libro: ",Libro5.Precio,"pesos")
+print("")
+print("")
+
+Libro6=BibliotecaBCS.BibliotecaUNAM("La Odisea","Homero",190)
+print("Nombre del libro: ",Libro6.Titulo)
+print("El autor es: ",Libro6.Autor)
+print("Costo del libro: ",Libro6.Precio,"pesos")
+print("")
+print("")
+
+Libro7=BibliotecaBCS.BibliotecaUNAM("La Iliada","Homero",150)
+print("Nombre del libro: ",Libro7.Titulo)
+print("El autor es: ",Libro7.Autor)
+print("Costo del libro: ",Libro7.Precio,"pesos")
+print("")
+print("")
+
+Libro8=BibliotecaBCS.BibliotecaUNAM("Las batallas en el desierto","Jose Emiliano Pacheco",210)
+print("Nombre del libro: ",Libro8.Titulo)
+print("El autor es: ",Libro8.Autor)
+print("Costo del libro: ",Libro8.Precio,"pesos")
+print("")
+print("")
+
+Libro9=BibliotecaBCS.BibliotecaUNAM("Psicomagia","Alejandro Jodorowsky",180)
+print("Nombre del libro: ",Libro9.Titulo)
+print("El autor es: ",Libro9.Autor)
+print("Costo del libro: ",Libro9.Precio,"pesos")
+print("")
+print("")
+
+Libro10=BibliotecaBCS.BibliotecaUNAM("El principito","Antonie de Saint Exupery",90)
+print("Nombre del libro: ",Libro10.Titulo)
+print("El autor es: ",Libro10.Autor)
+print("Costo del libro: ",Libro10.Precio,"pesos")
+print("")
+print("")
+
+print("Gracias,vuelve pronto")
