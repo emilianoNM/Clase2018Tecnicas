1a891f19ca70f36a6a910e5a3b819a0705b699bc
OliverMendieta <36421821+OliverMendieta@users.noreply.github.com>
2018-05-01 11:26:51 -0500

    Add files via upload

---
+++ Clase1402018/Biblioteca.py
@@ -0,0 +1,70 @@
+print "Tecnicas de programacion"
+print "Mendieta Montoya Oliver Salvador"
+print "Usuario github OliverMendieta\n"
+
+class Ciencia_ficcion:
+	titulo =None
+	autor = None
+	editorial = None
+	nopaginas = None
+	def __init__(self,titulo,autor,editorial,nopaginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.nopaginas=nopaginas
+	pass
+class Lit_clasica:
+	titulo =None
+	Autor = None
+	editorial = None
+	nopaginas = None
+	def __init__(self,titulo,autor,editorial,nopaginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.nopaginas=nopaginas
+	pass
+class Lit_clasica:
+	titulo =None
+	Autor = None
+	editorial = None
+	nopaginas = None
+	def __init__(self,titulo,autor,editorial,nopaginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.nopaginas=nopaginas
+	pass
+class Geologia:
+	titulo =None
+	Autor = None
+	editorial = None
+	nopaginas = None
+	def __init__(self,titulo,autor,editorial,nopaginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.nopaginas=nopaginas
+	pass
+class Matematicas:
+	titulo =None
+	Autor = None
+	editorial = None
+	nopaginas = None
+	def __init__(self,titulo,autor,editorial,nopaginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.nopaginas=nopaginas
+	pass
+class fisica:
+	titulo =None
+	Autor = None
+	editorial = None
+	nopaginas = None
+	def __init__(self,titulo,autor,editorial,nopaginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.nopaginas=nopaginas
+	pass
