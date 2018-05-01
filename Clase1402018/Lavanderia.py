a7e42b8094ed9a92b1c34678750c2345497b25c8
OliverMendieta <36421821+OliverMendieta@users.noreply.github.com>
2018-05-01 11:24:16 -0500

    Add files via upload

---
+++ Clase1402018/Lavanderia.py
@@ -0,0 +1,45 @@
+print "Tecnicas de programacion"
+print "Mendieta Montoya Oliver Salvador"
+print "Usuario github OliverMendieta\n"
+class Personal:
+	nombre=[]
+	estatura=None
+	edad=None
+	puesto=None
+	def __init__(self,nombre,edad,puesto,estatura):
+		self.nombre=nombre
+		self.edad=edad
+		self.estatura=estatura
+		self.puesto=puesto
+pass
+class Ropa:
+	
+	color = None
+	talla = None
+	genero = None
+	propietario =[]
+	def __init__(self,color,talla,genero,propietario):
+		self.color=color
+		self.talla=talla
+		self.genero=propietario
+		self.propietario=propietario
+pass
+class Lavadora:
+	tamano=None
+	precio=None
+	capacidad=None
+	def __init(self,tamano,genero,propietario):
+		self.tamano=tamano
+		self.precio=precio
+		self.capacidad=capacidad
+passs
+class Secadora:
+	tamano=None
+	capacidad=None
+	precio=None
+	def __init__(self,tamano,capacidad,precio)
+		self.tamano=tamano
+		self.capacidad=capacidad
+		self.precio=precio
+pass
+
