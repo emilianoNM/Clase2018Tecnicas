098e6cbcf209168a260ac2668db6f615833e9817
OliverMendieta <36421821+OliverMendieta@users.noreply.github.com>
2018-05-01 11:25:27 -0500

    Add files via upload

---
+++ Clase1402018/POA.py
@@ -0,0 +1,159 @@
+print "Tecnicas de programacion"
+print "Mendieta Montoya Oliver Salvador"
+print "Usuario github OliverMendieta\n"
+
+class Aguila :
+	nombre = []
+	estado = []
+	pico = None 
+	alas = None
+	piel = None
+	def __init__(self,nombre,estado,pico,alas,piel):
+		self.nombre = nombre
+		self.estado = estado
+		self.pico = pico
+		self.alas = alas
+		self.piel = piel
+	pass
+class Halcon :
+	nombre = []
+	estado = []
+	pico = None 
+	alas = None
+	piel = None
+	def __init__(self,nombre,estado,pico,alas,piel):
+		self.nombre = nombre
+		self.estado = estado
+		self.pico = pico
+		self.alas = alas
+		self.piel = piel
+	pass
+class canguro :
+	nombre= []
+	estado = []
+	color = None
+	tamano = None
+	orejas = None
+	def __init__(self,nombre,estado,color,tamano,orejas):
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.orejas = orejas
+	pass
+class Oso :
+	nombre= []
+	estado = []
+	color = None
+	tamano = None
+	garras = None
+	def __init__(self,nombre,estado,color,tamano,garras):
+
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.garras = garras
+	pass
+
+class Lobo :
+	nombre= []
+	estado = []
+	color = None
+	tamano = None
+	dientes = None
+	def __init__(self,nombre,estado,color,tamano,dientes):
+
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.dientes = dientes
+	pass
+
+
+
+class cocodrilo :
+	nombre = []
+	estado = []
+	color = None
+	tamano = None
+	dientes = None
+	def __init__(self,nombre,estado,color,tamano,dientes):
+
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.dientes = dientes
+	pass
+class Iguana :
+	
+	nombre = []
+	estado = []
+	color = None
+	tamano = None
+	alimentacion = "herbivoro"
+	
+	def __init__(self,nombre,estado,color,tamano,alimentacion = "herbivoro"):
+	
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.alimentacion = "herbivoro"
+	pass
+class Foca :
+	 
+	nombre = []
+	estado = []
+	color = None
+	tamano = None
+	alimentacion = "peces"
+
+	def __init__(self,nombre,estado,color,tamano,alimentacion = "peces"):
+	
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.alimentacion = "peces"
+	pass
+
+class Guacamaya :
+
+	nombre = []
+	estado = []
+	color = None
+	tamano = None
+	pico = None
+	alas = None
+	alimentacion = "herbivoro"
+
+	def __init__(self,nombre,estado,color,tamano,pico,alas,alimentacion = "herbivoro"):
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.alimentacion = "herbivoro"
+		self.pico = pico
+		self.alas = alas
+	pass
+
+class Rana : 
+
+	nombre = []
+	estado = []
+	color = None
+	tamano = None
+	alimentacion = "insectivoro"
+	def __init__(self,nombre,estado,color,tamano,alimentacion = "insectivoro"):
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.alimentacion = "insectivoro"
+	pass
+
+
+
