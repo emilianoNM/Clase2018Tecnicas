6cdae289b790186c1a1839f32fd9b20892d96289
NavGlez <36390188+NavGlez@users.noreply.github.com>
2018-05-30 01:11:13 -0500

    Add files via upload

---
+++ Clase1402018/NavGlez/bibliotecaNG.py
@@ -0,0 +1,79 @@
+
+class Historia:
+	titulo =None
+	autor = None
+	editorial = None
+	publicacion = None
+	paginas = None
+	def __init__(self,titulo,autor,editorial,publicacion,paginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.publicacion=publicacion
+		self.paginas=paginas
+	pass
+class Matematicas:
+	titulo =None
+	Autor = None
+	editorial = None
+	publicacion = None
+	paginas = None
+	def __init__(self,titulo,autor,editorial,publicacion,paginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.publicacion=publicacion
+		self.paginas=paginas
+	pass
+class Biologia:
+	titulo =None
+	Autor = None
+	editorial = None
+	publicacion = None
+	paginas = None
+	def __init__(self,titulo,autor,editorial,publicacion,paginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.publicacion=publicacion
+		self.paginas=paginas
+	pass
+class Geografia:
+	titulo =None
+	Autor = None
+	editorial = None
+	publicacion = None
+	paginas = None
+	def __init__(self,titulo,autor,editorial,publicacion,paginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.publicacion=publicacion
+		self.paginas=paginas
+	pass
+class Fisica:
+	titulo =None
+	Autor = None
+	editorial = None
+	publicacion = None
+	paginas = None
+	def __init__(self,titulo,autor,editorial,publicacion,paginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.publicacion=publicacion
+		self.paginas=paginas
+	pass
+class Civismo:
+	titulo =None
+	Autor = None
+	editorial = None
+	publicacion = None
+	paginas = None
+	def __init__(self,titulo,autor,editorial,publicacion,paginas):
+		self.titulo=titulo
+		self.autor=autor
+		self.editorial=editorial
+		self.publicacion=publicacion
+		self.paginas=paginas
+	pass
---
+++ Clase1402018/NavGlez/zooNG.py
@@ -0,0 +1,151 @@
+class Oso Pardo :
+	nombre = []
+	estado = []
+	garras = None 
+	pelo = None
+	piel = None
+	def __init__(self,nombre,estado,garras,pelo,piel):
+		self.nombre = nombre
+		self.estado = estado
+		self.garras = garras
+		self.pelo = pelo
+		self.piel = piel
+	pass
+class Panda :
+	nombre = []
+	estado = []
+	garras = None 
+	pelo = None
+	piel = None
+	def __init__(self,nombre,estado,garras,pelo,piel):
+		self.nombre = nombre
+		self.estado = estado
+		self.garras = garras
+		self.pelo = pelo
+		self.piel = piel
+	pass
+class Foca :
+	nombre= []
+	estado = []
+	tamano = None
+	nariz = None
+	color = None
+	def __init__(self,nombre,estado,tamano,nariz,color):
+		self.nombre = nombre
+		self.estado = estado
+		self.tamano = tamano
+		self.nariz = nariz
+		self.color = color
+	pass
+class Elefante :
+	nombre= []
+	estado = []
+	tamano = None
+	nariz = None
+	color = None
+	def __init__(self,nombre,estado,tamano,nariz,color):
+
+		self.nombre = nombre
+		self.estado = estado
+		self.tamano = tamano
+		self.nariz = nariz
+		self.color = color
+	pass
+
+class Rinoceronte :
+	nombre= []
+	estado = []
+	color = None
+	tamano = None
+	orejas = None
+	def __init__(self,nombre,estado,color,tamano,orejas):
+
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.orejas = orejas
+
+
+
+class Hipopotamo :
+	nombre = []
+	estado = []
+	color = None
+	tamano = None
+	orejas = None
+	def __init__(self,nombre,estado,color,tamano,orejas):
+
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.orejas = orejas
+	pass
+
+class Vibora :
+	
+	nombre = []
+	estado = []
+	color = None
+	tamano = None
+	piel = None
+	
+	def __init__(self,nombre,estado,color,tamano,piel):
+	
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.piel = piel
+	pass
+class Conejo :
+	 
+	nombre = []
+	estado = []
+	color = None
+	tamano = None
+	piel = None
+
+	def __init__(self,nombre,estado,color,tamano,piel):
+	
+		self.nombre = nombre
+		self.estado = estado
+		self.color = color
+		self.tamano = tamano
+		self.piel = piel
+	pass
+
+class Oso Polar :
+
+	nombre = []
+	estado = []
+	color = "blanco"
+	tamano = None
+	garras = None
+
+	def __init__(self,nombre,estado,color = 'blanco',tamano,garras):
+		self.nombre = nombre
+		self.estado = estado
+		self.color = "blaco"
+		self.tamano = tamano
+		self.garras = garras
+	pass
+
+class Zorro : 
+
+	nombre = []
+	estado = []
+	color = "rojo"
+	tamano = None
+	garras = None
+	def __init__(self,nombre,estado,color = 'rojo',tamano,garras):
+		self.nombre = nombre
+		self.estado = estado
+		self.color = "rojo"
+		self.tamano = tamano
+		self.garras = garras
+	pass
+
+
+
