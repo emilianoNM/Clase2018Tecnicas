8e8973619164ceeb065faa3a14e810ecf7719ee5
OliverMendieta <36421821+OliverMendieta@users.noreply.github.com>
2018-05-29 23:17:14 -0500

    Add files via upload

---
+++ Clase19022018/ZoologicoOliver.py
@@ -0,0 +1,156 @@
+class Animal:
+    Nombre=""
+    Nombrecientifico=""
+    Habitat=""
+class Oso(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def hibernar(self):
+        print("\nduerme todo el invierno\n")
+    def comer(self):
+        print("es omnivoro")
+    def trepar(self):
+        print("trepa arboles")
+class venado(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def correr(self):
+        print("corre por el bosque")
+    def comer():
+        print("es herbivoro")
+    def salta():
+        print("salta por el bosque")
+class guepardo(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def correr(self):
+        print("es el más rápido del mundo")
+    def comer(self):
+        print("es carnivoro")
+    def caza(self):
+        print("Caza en solitario")
+class leon(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def cazar(self):
+        print("caza en grupo")
+    def comer(self):
+        print("es carnivoro")
+    def dormir(self):
+        print("puede dormir por 20 horas seguidas")
+class delfin(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def nadar(self):
+        print("puede nadar")
+    def respirar(self):
+        print("no respira bajo el agua")
+    def saltar(self):
+        print("puede saltar sobre la superficie del agua")
+class mono(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def trepa(self):
+        print("trepa arboles")
+    def come(self):
+        print("come frutas")
+    def robar():
+        print("le gusta robar cosas")
+class aguila(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def volar(self):
+        print("puede volar")
+    def cazar(self):
+        print("caza desde los aires")
+    def come(self):
+        print("comer otros animales")
+class guacamaya(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def volar(self):
+        print("puede volar")
+    def imitar(self):
+        print("puede imitar sonidos")
+    def trepar(self):
+        print("puede trepar arboles con su pico")
+class lobo(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def cazar(self):
+        print("caza en grupo")
+class canguro(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def saltar(self):
+        print("salta por la pradera")
+    def pelar(self):
+        print("para defenderse se levanta en dos patas")
+    def vivir(Self):
+        print("vive en grupos")
+class nutria(Animal):
+    def __init__(self,Nombre,Nombrecientifico,Habitat):
+        self.Nombre=Nombre
+        self.Nombrecientifico=Nombrecientifico
+        self.Habitat=Habitat
+    def __str__(self):
+        cadena=self.Nombre+self.Nombrecientifico+self.Habitat
+        return cadena
+    def nadar(self):
+        print("puede nadar")
+    def caminar(self):
+        print("tambien camina en tierra")
+    def dormir(self):
+        print("duermen tomadas de la mano")
+
+
