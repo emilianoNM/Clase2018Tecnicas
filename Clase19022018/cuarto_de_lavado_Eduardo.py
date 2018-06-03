6ae86e07653b700b9c3f3448b9294077502852ce
Eduardo36 <36463790+Eduardo36@users.noreply.github.com>
2018-06-02 21:38:35 -0500

    Add files via upload

---
+++ Clase19022018/cuarto_de_lavado_Eduardo.py
@@ -0,0 +1,332 @@
+#Hernandez Quintana Luis Eduardo
+
+
+
+
+class Lavanderia():
+      Tipo_de_prenda=None
+      grado_de_suciedad=None
+      Color=None
+      
+
+class Prenda1(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 1:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color))
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
+
+  
+
+class Prenda2(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 2:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
+
+
+                
+
+class Prenda3(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 3:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
+
+class Prenda4(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 4:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
+
+class Prenda5(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 5:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
+
+
+
+                
+class Prenda6(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 6:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
+
+
+                
+
+class Prenda7(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 7:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
+
+
+
+                
+class Prenda8(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 8:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
+
+
+
+                
+
+class Prenda9(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 9:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
+
+
+
+                
+
+class Prenda10(Lavanderia):
+      Detergente_a_usar=None
+      tipo_de_ciclo=None
+      Tipo_de_tejido=None 
+      
+      def __init__(self,Tipo_de_prenda,grado_de_suciedad,Color,Detergente_a_usar,tipo_de_ciclo,Tipo_de_tejido):
+            self.Tipo_de_prenda=Tipo_de_prenda
+            self.grado_de_suciedad=grado_de_suciedad
+            self.Color=Color
+            self.Detergente_a_usar=Detergente_a_usar
+            self.tipo_de_ciclo=tipo_de_ciclo
+            self.Tipode_tejido=Tipode_tejido
+            
+      
+      def __str__(self):
+         return self.Tipo_de_prenda
+ 
+      def Info(self):
+          print('\n'"Características de la prenda numero 10:")
+          print("Prenda:",str(self.Tipo_de_prenda))
+          print('\n'"Nivel de suciedad:",str(self.grado_de_suciedad)
+          print('\n'"Color:",str(self.Color)) 
+        
+        
+      def Lavamos(self) :
+         print('\n'"Tus", str(self))
+         print("se lavaran con",str(self.Detergente_a_usar))
+         print("debido a que son de",str(self.Tipo_de_tejido))
+         print('\n'"Elegiste los siguientes servicios:",str(tipo_de_ciclo))
