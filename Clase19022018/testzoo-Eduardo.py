03f2676dd1664b8cda33f2619f7568835b401dec
Eduardo36 <36463790+Eduardo36@users.noreply.github.com>
2018-06-02 22:37:15 -0500

    Add files via upload

---
+++ Clase19022018/testzoo-Eduardo.py
@@ -0,0 +1,47 @@
+import zoo_Eduardo
+print ("")
+
+cas=zoo_Eduardo.castor("cas","cafe","vivo","2","shchhh","hostil","macho")
+cas.nada("3")
+cas.duerme("3")
+cas.caracteristica()
+
+
+ca=zoo_Eduardo.cabra("cas","cafe","vivo","2","shchhh","hostil","macho")
+ca.come("rapida")
+ca.duerme("3")
+
+
+
+
+agui=zoo_Eduardo.aguila("vivo","21","verde","Tiller","regular","","")
+agui.vuelamucho("solo para cazar")
+agui.caracteristicas()
+
+
+
+
+co=zoo_Eduardo.cocodrilo("vivi","4","verde","Mike","pequeno","","")
+co.nadar("lento")
+co.caracteristicas()
+
+piti=zoo_Eduardo.jirafa("vivo","7","amarilla y nagro","Piti","yerba","suave","hembra")
+piti.come("rapida")
+piti.duerme("3")
+piti.caracteristica()
+
+elsa=zooalvert1996.castor("vivo","15","gris","Elsa","fuerte","hostil","hembra")
+elsa.nada("12")
+elsa.duerme("10")
+elsa.caracteristica()
+
+dumbo=zoo_Eduardo.elefante("vivo","23","gris","Dumbo","grande","aspera","macho")
+dumbo.come(dumbo,"rapido")
+dumbo.duerme(dumbo,"5")
+dumbo.caracteristica()
+
+pupa=zoo_Eduardo.elefante("vivo","18","gris","Pupa","regular","aspera","hembra")
+pupa.come(pupa,"moderado")
+pupa.duerme(pupa,"10")
+pupa.caracteristica()
+
