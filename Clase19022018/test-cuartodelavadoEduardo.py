ebb99f9ed5eee0bc9683d19c889354626aac1327
Eduardo36 <36463790+Eduardo36@users.noreply.github.com>
2018-06-02 21:39:15 -0500

    Add files via upload

---
+++ Clase19022018/test-cuartodelavadoEduardo.py
@@ -0,0 +1,42 @@
+import cuarto_de_lavado_Eduardo
+
+
+Prenda1=cuarto_de_lavado_Eduardo.Prenda1("Jeans ","Muy sucios","Color","Detergente liquido","\nLavado \nSecado","algodón")
+Prenda1.Info()
+Prenda1.Lavamos()
+
+Prenda2=cuarto_de_lavado_Eduardo.Prenda2("Camisa ","Poco sucia","Color","Detergente liquido","\nLavado \nSecado \nPlanchado ","algodón")
+Prenda2.Info()
+Prenda2.Lavamos()
+
+Prenda3=cuarto_de_lavado_Eduardo.Prenda3("Playera ","Muy sucia","Blanca","Detergente en polvo","\nLavado \nSecado","algodón")
+Prenda3.Info()
+Prenda3.Lavamos()
+
+Prenda4=cuarto_de_lavado_Eduardo.Prenda4("Chamarra","Poco sucia","Color","Detergente liquido","\nLavado \nSecado","poliéster")
+Prenda4.Info()
+Prenda4.Lavamos()
+
+Prenda5=cuarto_de_lavado_Eduardo.Prenda5("Playera deportiva ","sucia","Color","Detergente en polvo","\nLavado \nSecado","poliéster")
+Prenda5.Info()
+Prenda5.Lavamos()
+
+Prenda6=cuarto_de_lavado_Eduardo.Prenda6("Calcetines ","Muy sucios","Blanco","Detergente en polvo","\nLavado \nSecado","algodon")
+Prenda6.Info()
+Prenda6.Lavamos()
+
+Prenda7=cuarto_de_lavado_Eduardo.Prenda7("Bata ","Demasiado sucioa","Blanco","Detergente liquido","\nLavado \nSecado \nPlanchado ","algodon")
+Prenda7.Info()
+Prenda7.Lavamos()
+
+Prenda8=cuarto_de_lavado_Eduardo.Prenda8("Sabana ","Poco sucia","Color","Detergente en polvo","\nLavado \nSecado","algodon")
+Prenda8.Info()
+Prenda8.Lavamos()
+
+Prenda9=cuarto_de_lavado_Eduardo.Prenda9("Cobija ","Muy sucia","Color","Detergente en polvo","\nLavado \nSecado","algodon")
+Prenda9.Info()
+Prenda9.Lavamos()
+
+Prenda10=cuarto_de_lavado_Eduardo.Prenda10("Ropa de bebé ","Poco sucioa","Color","Detergente liquido y suavizante","\nLavado \nSecado \nPlanchado ","algodon")
+Prenda10.Info()
+Prenda10.Lavamos()
