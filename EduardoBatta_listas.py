c3acac37d234384d9bb29da770f3bce7511d6d96
Eduardo Batta González <36352244+Eduardo-Batta@users.noreply.github.com>
2018-06-04 21:09:53 -0500

    Create EduardoBatta_listas.py

---
+++ EduardoBatta_listas.py
@@ -0,0 +1,7 @@
+#Eduardo Daniel Batta González
+
+#CREAR UNA LISTA DE 1-10 Y QUE IMPRIMA LOS VALORES MAYORES A 5
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in range(10):
+	if a [i] > 5:
+	print(a[i])
