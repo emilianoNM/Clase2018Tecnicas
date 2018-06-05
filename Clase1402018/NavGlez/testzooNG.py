acc75cc233d2da739f5bc977996c8fd99d28baad
NavGlez <36390188+NavGlez@users.noreply.github.com>
2018-06-04 20:51:59 -0500

    Add files via upload

---
+++ Clase1402018/NavGlez/testzooNG.py
@@ -0,0 +1,18 @@
+import zooNG
+
+A1=zooNG.Panda("Panda","En peligro de extincion","si","suave","rosadito")
+print(A1.nombre)
+print("Estado: ",A1.estado)
+print("Garras: ",A1.garras)
+print("Pelo: ",A1.pelo)
+print("Piel: ",A1.piel)
+print("")
+
+
+A2=zooNG.Foca("Foca","En peligro de extincion","grande","si","varia segun la especie")
+print(A2.nombre)
+print("Estaso: ",A2.estado)
+print("Su tamano es: ",A2.tamano)
+print("Tiene nariz",A2.nariz)
+print("Color",A2.color)
+print("")
