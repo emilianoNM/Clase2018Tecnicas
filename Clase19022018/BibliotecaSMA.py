759d3fad1b18320bd0aefe8c217d8fe7fd8890e2
TonySantes7 <36575302+TonySantes7@users.noreply.github.com>
2018-06-05 22:09:32 -0500

    Add files via upload

---
+++ Clase19022018/BibliotecaSMA.py
@@ -0,0 +1,65 @@
+# Santes Mejia Antonio
+print "...Tecnicas de programacion.."
+
+class Español:
+titulo =None
+autor = None
+editorial = None
+Precio = None
+def __init__(self,titulo,autor,editorial,Precio):
+	self.titulo=titulo
+	self.autor=autor
+	self.editorial=editorial
+	self.Precio=Precio
+pass
+
+
+class Matematicas:
+titulo =None
+autor = None
+editorial = None
+Precio = None
+def __init__(self,titulo,autor,editorial,Precio):
+	self.titulo=titulo
+	self.autor=autor
+	self.editorial=editorial
+	self.Precio=Precio
+pass
+
+
+class Geografia:
+titulo =None
+autor = None
+editorial = None
+Precio = None
+def __init__(self,titulo,autor,editorial,Precio):
+	self.titulo=titulo
+	self.autor=autor
+	self.editorial=editorial
+	self.Precio=Precio
+pass
+
+class Ciencias Naturales:
+titulo =None
+autor = None
+editorial = None
+Precio = None
+def __init__(self,titulo,autor,editorial,Precio):
+	self.titulo=titulo
+	self.autor=autor
+	self.editorial=editorial
+	self.Precio=Precio
+pass
+
+class Civismo:
+titulo =None
+autor = None
+editorial = None
+Precio = None
+def __init__(self,titulo,autor,editorial,Precio):
+	self.titulo=titulo
+	self.autor=autor
+	self.editorial=editorial
+	self.Precio=Precio
+pass
+
