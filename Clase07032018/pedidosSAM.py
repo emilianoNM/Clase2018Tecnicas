20e674bae8b7ec2fa24762f12fca2cb1caf7d149
TonySantes7 <36575302+TonySantes7@users.noreply.github.com>
2018-06-05 22:54:50 -0500

    Add files via upload

---
+++ Clase07032018/pedidosSAM.py
@@ -0,0 +1,28 @@
+# Santes Mejia Antonio
+print "...Tecnicas de programacion.."
+
+print("Fruta disponible:Sandia,Naranja,Guayaba,Melon,Planatano,Mandarina")
+
+lista de fruta=[]
+
+def Pedido(x):
+  lista de fruta.append(x)
+pass
+
+def Lista():
+  print "Lista de Fruta:", lista
+pass
+
+def Entrega():
+print("Fruta\nCantidad pedida")
+  Sa=lista.count("Sandia")
+print "Sandia", Sa
+  Na=lista.count("Naranja")
+print "Naranja", Na
+  Gu=lista.count("Guayaba")
+print "Guayaba", Na
+  Me=lista.count("Melon")
+print "Melon", Me
+  Ma=lista.count("Mandarina")
+print "Mandarina", Ma
+
